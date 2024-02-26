8d573104edf9efac09ea2d528727effcb4012f28
Your Name <you@example.com>
2024-02-26 13:08:48 +0530

    change

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -52,7 +52,7 @@ const Home = () => {
     e.preventDefault();
     try {
       console.log(formData);
-      axios.post("http://localhost:8081/submit-form", formData);
+      axios.post("https://emp-task-7.onrender.com/submit-form", formData);
       toast.success("Employee data submitted successfully!");
     } catch (error) {
       console.error("Error:", error);
